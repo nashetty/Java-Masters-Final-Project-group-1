21f8abd4a16cda0eec98c9ee07aea400a8cab73d
Amal Aden <al.co@hotmail.com>
2024-07-11 12:30:44 +0100

    intial commit

---
+++ src/main/java/com/codefirstgirls/green_energy_manager/GreenEnergyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.codefirstgirls.green_energy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenEnergyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenEnergyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefirstgirls/green_energy_manager/GreenEnergyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefirstgirls.green_energy_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenEnergyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
