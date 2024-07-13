d416d878454c4d6afea9326c9b2cbeb99632e377
China Pickering <chinapickering97@gmail.com>
2024-07-13 22:16:58 +0100

    added Energy class with preliminary names for columns before sql table created

---
+++ src/main/java/com/codefirstgirls/green_energy_manager/Energy.java
@@ -0,0 +1,27 @@
+package com.codefirstgirls.green_energy_manager;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="energy")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Energy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="energyType")
+    private String energyType;
+
+    @Column(name="transactionType")
+    private String transactionType;
+
+    @Column(name="amount_K_W")
+    private double amount_K_W;
+}
