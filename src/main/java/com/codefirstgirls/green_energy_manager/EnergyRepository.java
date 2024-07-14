1e619f5fc8a09ab7b567c4141ed5425441851343
Amal Aden <al.co@hotmail.com>
2024-07-14 07:45:22 +0100

    Added EnergyRepository interface to the project

---
+++ src/main/java/com/codefirstgirls/green_energy_manager/EnergyRepository.java
@@ -0,0 +1,10 @@
+package com.codefirstgirls.green_energy_manager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EnergyRepository extends JpaRepository<Energy, Integer> {
+    List<Energy> findByCustomerId(Integer id);
+}
+
