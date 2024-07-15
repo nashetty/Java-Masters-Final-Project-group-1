ca04a6c799b84617226af26e6f079606b4f16666
Amal Aden <al.co@hotmail.com>
2024-07-15 13:42:32 +0100

    Renamed the method to findByTransaction in   EnergyRepository interface

--- src/main/java/com/codefirstgirls/green_energy_manager/EnergyRepository.java
+++ src/main/java/com/codefirstgirls/green_energy_manager/EnergyRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EnergyRepository extends JpaRepository<Energy, Integer> {
-    List<Energy> findByCustomerId(Integer id);
+    List<Energy> findByTransactionId(Integer id);
 }
 
