945e32e738b75b713a2ed7cf0004e54f58128165
Amal Aden <al.co@hotmail.com>
2024-07-18 20:25:31 +0100

    - Updated EnergyController which now contains a get endpoint that gets rounded net energy difference calculation and a response messages. - Also, response JSON  includes more user-friendly month names. -  The updated EnergyRepository query method can now fetch the total energy transaction ,grouped by transaction type.

--- src/main/java/com/codefirstgirls/green_energy_manager/controller/EnergyController.java
+++ src/main/java/com/codefirstgirls/green_energy_manager/controller/EnergyController.java
@@ -6,10 +6,14 @@ import com.codefirstgirls.green_energy_manager.model.repository.EnergyRepository
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Month;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController // scanning by spring
 @Slf4j // for logging
@@ -55,5 +59,58 @@ public class EnergyController {
     //        log.info("Retrieved energy type: " + getEnergyFirst);
     //        return  ResponseEntity.ok("Energy type returned: " + getEnergyFirst );
     //    }
+
+
+
+     // GET endpoint which calculates the net energy difference for a specified month.
+
+    @GetMapping("/netMonthlyEnergyDifference")
+    public ResponseEntity<Map<String, Object>> netEnergyDifferenceCalculation(@RequestParam int month) {
+        try {
+            List<Object[]> monthlyTotals = energyRepository.findTotalMonthlyEnergy(month);
+
+
+            // initialise both variables.
+            double totalGenerated = 0;
+            double totalUsed = 0;
+
+            // Iteration to help calculate generated and used energy
+            for (Object[] total : monthlyTotals) {
+                String transactionType = (String) total[0];
+                double amount = ((Number) total[1]).doubleValue();
+
+                if ("generated".equals(transactionType)) {
+                    totalGenerated = amount;
+                } else if ("used".equals(transactionType)) {
+                    totalUsed = amount;
+                }
+            }
+            // Calculate the net energy difference
+            double netEnergyDifference = totalGenerated - totalUsed;
+            // This method is  for rounding the net energy difference calculation.
+            long roundedNetEnergyDifference = Math.round(netEnergyDifference);
+
+            // A method to get the month's name based on the month number to improve readability for user.
+            String monthName = Month.of(month).name();
+
+            // This response message will be displayed to the user.
+            String message = "Net Energy Difference for " + monthName + " is: " + roundedNetEnergyDifference + " kWh";
+
+            // Structure of the message for displaying  the calculated value of the  Net Energy Difference and a descriptive message to the user.
+            Map<String, Object> responseMessage = new HashMap<>();
+            responseMessage.put("netEnergyDifference", message);
+
+            return ResponseEntity.ok(responseMessage);
+        } catch (Exception e) {
+            log.error("Failed to calculate the net energy difference for the month of " + month, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
+
+//  A Link to test @GetMapping endpoint:
+//  http://localhost:8080/api/energy/netMonthlyEnergyDifference?month=4
+//  Append the required month number as a query parameter to the end of the URL.
+// Swap '4' with the month number that the calculation of the net energy difference should be for.
+
 
